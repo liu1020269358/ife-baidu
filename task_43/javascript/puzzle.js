2ab059f393dd09ff84dda0431a3e32de5d49da45
liu <1020269358@qq.com>
2016-05-24 23:14:45 +0800

    task_43

---
+++ task_43/javascript/puzzle.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+    init()
+}
+
+function init(){
+    a = $('.album');
+    a.map(function(){
+        var _this = jQuery(this);
+        num = _this.children().length;
+
+        switch(num){
+            case 1:
+            _this.addClass('album-1');
+            break;
+            case 2:
+            _this.addClass('album-2');
+            break;
+            case 3:
+            _this.addClass('album-3');
+            break
+            case 4:
+            _this.addClass('album-4');
+            break;
+            case 5:
+            _this.addClass('album-5');
+            break;
+            case 6:
+            _this.addClass('album-6');
+            break;
+        }
+        
+    })
+}
+
